f22069256c06ddca88af963545171c4fcd1e11d7
Ngoakoana <ngoakoana.mphago460@gmail.com>
2024-10-08 14:47:05 +0200

    changed the contact and landing pages

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LandingComponent } from './pages/landing/landing.component';
 export const routes: Routes = [
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'landing', component: LandingComponent },
-    { path: 'contract', component: ContractComponent },
+    { path: 'contract', component: ContractComponent},
     { path: 'users', component: ContractComponent } 
 ];
 
--- app/src/app/pages/contract/contract.component.ts
+++ app/src/app/pages/contract/contract.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contract',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './contract.component.html',
   styleUrl: './contract.component.css'
 })
