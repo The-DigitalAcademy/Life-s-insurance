7a62efbdbb3dd58a3d67e577fe7f828abb78f3bf
Ngoakoana <ngoakoana.mphago460@gmail.com>
2024-10-08 12:15:54 +0200

    added routes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ContractComponent } from './pages/contract/contract.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'landing', pathMatch: 'full' },
+    { path: 'landing', component: LandingComponent },
+    { path: 'contract', component: ContractComponent },
+    { path: 'users', component: ContractComponent } 
+];
+
