2dd06f34f9d0deb26235adc240bf91e6ebcb8041
Nellark <nelakulati@gmail.com>
2024-10-08 16:15:19 +0200

    added registration form

--- app/src/app/pages/business/business.component.ts
+++ app/src/app/pages/business/business.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-business',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './business.component.html',
   styleUrl: './business.component.css'
 })
 export class BusinessComponent {
 
+  selectedPlan: string = '';
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  };
+firstName: any;
+lastName: any;
+email: any;
+password: any;
+
+ 
+  onSubmit() {
+    if (this.selectedPlan && this.formData.firstName && this.formData.email && this.formData.password) {
+      console.log('Form Submitted', this.formData, 'Selected Plan:', this.selectedPlan);
+    
+    } else {
+      console.error('Form is incomplete');
+    }
+  }
+
+
 }
+
+
+
+
