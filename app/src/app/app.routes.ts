f9014c6d894963c946f973df887ebdc7058f8edd
reeri15 <reshay.makunyane@gmail.com>
2024-10-08 16:30:03 +0200

    added bottom nav

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavComponent } from './nav/nav.component';
 
 export const routes: Routes = [
 
     {
         path: "login",
         component: LoginComponent,
+    },
+
+    {
+path: "profile",
+component: ProfileComponent,
+
+    },
+
+    {
+path: "nav",
+component: NavComponent,
     },
 ];
