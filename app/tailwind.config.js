64af5b952fba696562983e9d0343fc170693db04
Nellark <nelakulati@gmail.com>
2024-10-09 15:24:37 +0200

    added logic on the database

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{html,ts}', // Adjust this according to your project's structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
