a225a2ad523b895926ad3f85b545dbd13f36f1a2
Nellark <nelakulati@gmail.com>
2024-10-09 14:17:30 +0200

    Resolved merge conflict in app.routes.ts

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -4,9 +4,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { NavComponent } from './nav/nav.component';
 import { ContractComponent } from './pages/contract/contract.component';
 import { LandingComponent } from './pages/landing/landing.component';
+<<<<<<< Updated upstream
 import {  HomeInsurerComponent } from './homeInsurer/homeInsurer.component';
 import { BusinessComponent } from './pages/business/business.component';
 import { HomeComponent } from './pages/home/home.component';
+=======
+import { HomeComponent } from './pages/home/home.component';
+import { BusinessComponent } from './pages/business/business.component';
+>>>>>>> Stashed changes
 
 export const routes: Routes = [
 
@@ -16,14 +21,26 @@ export const routes: Routes = [
     {path: "nav",component: NavComponent,},
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'landing', component: LandingComponent },
+    { path: 'home', title: 'home',component: HomeComponent },
     { path: 'contract', component: ContractComponent},
+<<<<<<< Updated upstream
     { path: 'users', component: ContractComponent } ,
    {path:'homeInsurer',component:HomeInsurerComponent},
     {path: 'business', title: 'Business', component: BusinessComponent},
  
+=======
+    { path: 'users', component: ContractComponent },
+    {path: 'business', title: 'Business', component: BusinessComponent}, 
+    
+
+
+>>>>>>> Stashed changes
 
 
 ];
 
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
