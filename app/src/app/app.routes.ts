8913608c9040b75c8710861cf50da665aea731a3
Nellark <nelakulati@gmail.com>
2024-10-09 13:15:46 +0200

    pulled the changes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -4,38 +4,22 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { BusinessComponent } from './pages/business/business.component';
 import { HomeComponent } from './pages/home/home.component';
-
-
-
-
-
-import { ContractComponent } from './pages/contract/contract.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import {  HomeInsurerComponent } from './homeInsurer/homeInsurer.component';
-import { BusinessComponent } from './pages/business/business.component';
-import { HomeComponent } from './pages/home/home.component';
-import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeInsurerComponent } from './homeInsurer/homeInsurer.component';
+
 
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'landing', component: LandingComponent },
-
     { path: 'contact', component: ContactComponent },
     { path: 'users', component: ContactComponent }, 
     { path: 'business', title: 'Business', component: BusinessComponent },
-    { path: 'home', title: 'Home', component: HomeComponent }, 
-];
-
-    { path: 'contract', component: ContractComponent},
+    { path: 'home', title: 'Home', component: HomeComponent}, 
     { path: 'login', component: LoginComponent } ,
-    { path: 'home', component: HomeComponent } ,
-   {path:'homeInsurer',
-    component:HomeInsurerComponent},
-
-{path: 'business', title: 'Business', component: BusinessComponent},
+    {path:'homeInsurer',component:HomeInsurerComponent},
+    
 
 
 
