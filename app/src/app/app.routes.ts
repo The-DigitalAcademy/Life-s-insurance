b2e7fc2db8bb1df440d0608034ac0cceaca4e26d
Nellark <nelakulati@gmail.com>
2024-10-09 14:26:15 +0200

    added components

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -4,14 +4,10 @@ import { ProfileComponent } from './profile/profile.component';
 import { NavComponent } from './nav/nav.component';
 import { ContractComponent } from './pages/contract/contract.component';
 import { LandingComponent } from './pages/landing/landing.component';
-<<<<<<< Updated upstream
 import {  HomeInsurerComponent } from './homeInsurer/homeInsurer.component';
 import { BusinessComponent } from './pages/business/business.component';
 import { HomeComponent } from './pages/home/home.component';
-=======
-import { HomeComponent } from './pages/home/home.component';
-import { BusinessComponent } from './pages/business/business.component';
->>>>>>> Stashed changes
+
 
 export const routes: Routes = [
 
@@ -23,24 +19,9 @@ export const routes: Routes = [
     { path: 'landing', component: LandingComponent },
     { path: 'home', title: 'home',component: HomeComponent },
     { path: 'contract', component: ContractComponent},
-<<<<<<< Updated upstream
     { path: 'users', component: ContractComponent } ,
    {path:'homeInsurer',component:HomeInsurerComponent},
     {path: 'business', title: 'Business', component: BusinessComponent},
  
-=======
-    { path: 'users', component: ContractComponent },
-    {path: 'business', title: 'Business', component: BusinessComponent}, 
-    
-
-
->>>>>>> Stashed changes
-
 
 ];
-
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
