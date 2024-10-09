a13d6272b8d6d82ddf82e5f16b623b4b6a4ac308
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-10-09 08:56:32 +0200

    changed paragraph position

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import {  HomeInsurerComponent } from './homeInsurer/homeInsurer.component';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        component:HomeComponent,
-    }
+    
+
+   {path:'homeInsurer',
+    component:HomeInsurerComponent
+
+   }
 ];
---
+++ app/src/app/homeInsurer/homeInsurer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './homeInsurer.component.html',
+  styleUrl: './homeInsurer.component.css'
+})
+export class HomeInsurerComponent{
+
+}
