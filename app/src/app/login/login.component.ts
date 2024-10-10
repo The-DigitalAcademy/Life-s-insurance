9b0d08d33330bc98fa6e8de36d097eb9ef2e6fdd
reeri15 <reshay.makunyane@gmail.com>
2024-10-10 10:43:54 +0200

    fixed

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +7,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
-apiURL ='http://localhost:3000/profile';
-
-
-email: string = '';
-password: string = '';
-
-
-
-  ngOnInit(): void {
-  }
-
+export class LoginComponent {
 
 }
-
