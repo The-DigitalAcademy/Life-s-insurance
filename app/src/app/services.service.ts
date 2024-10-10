17c279577fe95523747b570e6ab7d3028559c7bb
Nellark <nelakulati@gmail.com>
2024-10-10 09:38:43 +0200

    added services

--- app/src/app/services.service.ts
+++ app/src/app/services.service.ts
@@ -1,53 +1,9 @@
-import { inject, Injectable } from '@angular/core';
-
-import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  login(arg0: { email: string; password: string; }) {
-    throw new Error('Method not implemented.');
-  }
-  read() {
-    throw new Error('Method not implemented.');
-  }
-  users: UsersInterface[] = [];
-  router = inject(Router)
-
+export class ServicesService {
 
   constructor() { }
-
-  onRegister(userData: UsersInterface) {
-    const userEmail = userData.email;
-    console.log(userData);
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].email === userEmail){
-        alert(`${userEmail} is taken`);
-        return
-      }
-    }
-
-     this.users.push(userData);
-     
-     alert(`${userData.username} was registered successfully`)
-     this.router.navigate(['/login'])
-      
-  }
-
- signIn(userData: UsersInterface){
-   console.log(userData);
-
-  for (let i = 0; i < this.users.length; i++) {
-
-    if(this.users[i].email === userData.email && this.users[i].password === userData.password){
-      alert(`Welcome ${this.users[i].username}`)
-      this.router.navigateByUrl('/home')
-    }
-    else{
-      alert(`wrong details`)
-    } 
-  }
-  }
 }
- 
