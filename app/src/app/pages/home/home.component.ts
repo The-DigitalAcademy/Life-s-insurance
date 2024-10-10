c504e1bff2c6e4a5f69ca8ff7068cd70fac7a8ad
lifelebisi <life.lebisi20@gmail.com>
2024-10-10 08:31:05 +0200

    linked login page

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../UI/shared--UI/navbar/navbar.component";
+import { NavComponent } from "../../nav/nav.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NavComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
