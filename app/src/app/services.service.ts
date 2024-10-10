703aaafe38e5d7bf73ef2618ca3c873823958e6c
Nellark <nelakulati@gmail.com>
2024-10-10 09:13:50 +0200

    added services

---
+++ app/src/app/services.service.ts
@@ -0,0 +1,53 @@
+import { inject, Injectable } from '@angular/core';
+import { UsersInterface } from '../types/users';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  login(arg0: { email: string; password: string; }) {
+    throw new Error('Method not implemented.');
+  }
+  read() {
+    throw new Error('Method not implemented.');
+  }
+  users: UsersInterface[] = [];
+  router = inject(Router)
+
+
+  constructor() { }
+
+  onRegister(userData: UsersInterface) {
+    const userEmail = userData.email;
+    console.log(userData);
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].email === userEmail){
+        alert(`${userEmail} is taken`);
+        return
+      }
+    }
+
+     this.users.push(userData);
+     
+     alert(`${userData.username} was registered successfully`)
+     this.router.navigate(['/login'])
+      
+  }
+
+ signIn(userData: UsersInterface){
+   console.log(userData);
+
+  for (let i = 0; i < this.users.length; i++) {
+
+    if(this.users[i].email === userData.email && this.users[i].password === userData.password){
+      alert(`Welcome ${this.users[i].username}`)
+      this.router.navigateByUrl('/home')
+    }
+    else{
+      alert(`wrong details`)
+    } 
+  }
+  }
+}
+ 
